ffcb47be4003c3f4e059e04b73e59ceca8dbcd39
ronchuxia <ronchuxia@outlook.com>
2026-01-19 23:13:22 -0500

    add world 2 clip transformation

--- Tutorial.hpp
+++ Tutorial.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "PosColVertex.hpp"
+#include "mat4.hpp"
 
 #include "RTG.hpp"
 
@@ -91,6 +92,8 @@ struct Tutorial : RTG::Application {
 	virtual void on_input(InputEvent const &) override;
 
 	float time = 0.0f;
+
+	mat4 CLIP_FROM_WORLD;
 
 	std::vector< LinesPipeline::Vertex > lines_vertices;
 
